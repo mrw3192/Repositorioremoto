4d02aaf5de824f42464b3fc849c7c490a8b1521c
Marce Wain <mwain@live.com>
2023-06-05 19:40:10 -0300

    cambiar color de fondo en la rama cambios-fondo-1

---
+++ index.js
@@ -0,0 +1,9 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick() { 
+
+    alert('¡Hola! has hecho click en el boton.');
+}
+
+miBoton.addEventListener('click', handleClick);
+
+
